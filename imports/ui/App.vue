ac2fb436d8233aac590ee2e56157fd63b817494c
AkmElias <akmelias11@gmail.com>
2020-10-26 11:22:11 +0600

    eteor vue integrated successfully

---
+++ imports/ui/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <Students v-bind:students="students" />
+  </div>
+</template>
+
+<script>
+import Students from "./Students.vue";
+
+export default {
+  components: {
+    Students,
+  },
+  data() {
+    return {
+      students: [
+        {
+          name: "Elias",
+          email: "elias1@gmail.com",
+          phone: 01879023909,
+          DateOfBirth: "31 Dec 1994",
+          Subject: "English",
+        },
+        {
+          name: "Elias2",
+          email: "elias1@gmail.com",
+          phone: 01879023909,
+          DateOfBirth: "31 Dec 1994",
+          Subject: "English",
+        },
+        {
+          name: "Elias3",
+          email: "elias1@gmail.com",
+          phone: 01879023909,
+          DateOfBirth: "31 Dec 1994",
+          Subject: "English",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
---
+++ imports/ui/Students.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="student-row">
+      <h4>Name</h4>
+      <h4>Email</h4>
+      <h4>Phone</h4>
+      <h4>DateOfBirth</h4>
+    </div>
+    <div class="student" v-for="student in students">
+      <p>{{ student.name }}</p>
+      <p>{{ student.email }}</p>
+      <p>{{ student.phone }}</p>
+      <p>{{ student.DateOfBirth }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Students",
+  props: ["students"],
+};
+</script>
+
+<style scoped>
+.student-row {
+  display: flex;
+  justify-content: space-around;
+}
+.student {
+  display: flex;
+  justify-content: space-around;
+  background-color: gray;
+}
+</style>
